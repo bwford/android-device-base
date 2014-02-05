386907f8e94be2d7a3a5f529e1f2fdc8fd3524db
Paul Soucy <paul@diskless.(none)>
2014-02-05 14:02:35 -0500

    getting ready to try a build

--- full_libz500.mk
+++ full_libz500.mk
@@ -1,5 +1,71 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base.mk)
+PRODUCT_PACKAGES := \
+    DeskClock \
+    Bluetooth \
+    Calculator \
+    Calendar \
+    CertInstaller \
+    DrmProvider \
+    Email2 \
+    Exchange2 \
+    FusedLocation \
+    Gallery2 \
+    InputDevices \
+    LatinIME \
+    Launcher2 \
+    Music \
+    MusicFX \
+    Provision \
+    QuickSearchBox \
+    Settings \
+    SystemUI \
+    CalendarProvider \
+    bluetooth-health \
+    hostapd \
+    wpa_supplicant.conf
+
+PRODUCT_PACKAGES += \
+    audio \
+    dhcpcd.conf \
+    network \
+    pand \
+    pppd \
+    sdptool \
+    wpa_supplicant
+
+PRODUCT_PACKAGES += \
+    icu.dat
+
+PRODUCT_PACKAGES += \
+    librs_jni \
+    libvideoeditor_jni \
+    libvideoeditor_core \
+    libvideoeditor_osal \
+    libvideoeditor_videofilters \
+    libvideoeditorplayer \
+
+PRODUCT_PACKAGES += \
+    audio.primary.default \
+    audio_policy.default \
+    local_time.default \
+    power.default
+
+PRODUCT_PACKAGES += \
+    local_time.default
+
+PRODUCT_COPY_FILES := \
+        frameworks/av/media/libeffects/data/audio_effects.conf:system/etc/audio_effects.conf
+
+$(call inherit-product-if-exists, frameworks/base/data/fonts/fonts.mk)
+$(call inherit-product-if-exists, external/cibu-fonts/fonts.mk)
+$(call inherit-product-if-exists, external/lohit-fonts/fonts.mk)
+$(call inherit-product-if-exists, external/naver-fonts/fonts.mk)
+$(call inherit-product-if-exists, frameworks/base/data/keyboards/keyboards.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core.mk)
+
+$(call inherit-product, hardware/ti/omap4xxx/omap4.mk)
+$(call inherit-product, frameworks/native/build/tablet-dalvik-heap.mk)
+
+$(call inherit-product-if-exists, device/ti/proprietary-open/omap4/ti-omap4-vendor.mk)
 
 DEVICE_PACKAGE_OVERLAYS :=
 
